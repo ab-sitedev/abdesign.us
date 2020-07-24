ec6ab694abf65bb5a9281a4d0e788eb629ecbd36
Aaron Banister <ab.sitedev@gmail.com>
2020-07-23 22:03:23 -0500

    Initial

---
+++ asset/js/nav.js
@@ -0,0 +1,13 @@
+// Toggle via classList.toggle
+function toggleNav() {
+    var menu = document.getElementById("my-sidenav").classList.toggle("toggle");
+}
+
+function closeNav() {
+    
+    if (document.getElementById('my-sidenav').classList.contains("toggle")){
+        document.getElementById('menu_label').click();
+    }
+    
+}
+  
