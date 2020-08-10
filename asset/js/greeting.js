ab73c76762a62223f4e82cd397ae7c151664e5c7
Aaron Banister <ab.sitedev@gmail.com>
2020-08-10 10:04:23 -0500

    Add bio with dynamic time-based greeting

---
+++ asset/js/greeting.js
@@ -0,0 +1,20 @@
+var now = new Date();
+    var hours = now.getHours();
+    var ft = now.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true
+    });
+    if (0 <= hours && hours < 12) {//Morning
+        
+        var type = "morning";
+    }
+    if (12 <= hours && hours < 17) {//Afternoon
+        
+        var type = "afternoon";
+    }
+    if (17 <= hours && hours < 0) {//Evening
+        
+        var type = "evening";
+    }
+    document.getElementById("greeting").innerHTML = `Good ${type},`;
